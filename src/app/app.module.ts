412ef659aa82fa67eeac47a209a3077eb8d346e7
Anushka-04 <anushkabhattarai04@gmail.com>
2021-08-07 19:25:25 +0545

    Button component added to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
