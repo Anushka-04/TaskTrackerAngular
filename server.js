867373ff1fd8146a121ccda45ba1b0d6ec190b0f
Anushka-04 <anushkabhattarai04@gmail.com>
2021-08-11 17:08:59 +0545

    test commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/todoapp'));
+app.use(express.static(`./frontend/dist/todoapp`));
 
 app.get('*',(req,res) => {
     res.sendFile(`./front-end/dist/index.html`);
