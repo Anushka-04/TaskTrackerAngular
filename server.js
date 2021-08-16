17b11b0e56a7a8c008c35355ea4ed915f0e3999b
Anushka-04 <anushkabhattarai04@gmail.com>
2021-08-16 20:44:41 +0545

    deploy to heroku settings

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(`./frontend/dist/todoapp`));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
 
-app.get('*',(req,res) => {
-    res.sendFile(`./front-end/dist/index.html`);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
